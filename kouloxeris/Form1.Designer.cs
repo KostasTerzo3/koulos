dcbe8ee21dd94be7e90f321f47a03d0c5b1352c6
Kostas <kostastennis1998@gmail.com>
2018-01-26 10:46:29 +0200

    Evala eikonitses kai koumpakia lul

--- kouloxeris/Form1.Designer.cs
+++ kouloxeris/Form1.Designer.cs
@@ -28,12 +28,349 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.seven1 = new System.Windows.Forms.PictureBox();
+            this.cherry1 = new System.Windows.Forms.PictureBox();
+            this.seven2 = new System.Windows.Forms.PictureBox();
+            this.seven3 = new System.Windows.Forms.PictureBox();
+            this.seven4 = new System.Windows.Forms.PictureBox();
+            this.cherry3 = new System.Windows.Forms.PictureBox();
+            this.cherry2 = new System.Windows.Forms.PictureBox();
+            this.cherry4 = new System.Windows.Forms.PictureBox();
+            this.grape3 = new System.Windows.Forms.PictureBox();
+            this.grape4 = new System.Windows.Forms.PictureBox();
+            this.grape2 = new System.Windows.Forms.PictureBox();
+            this.grape1 = new System.Windows.Forms.PictureBox();
+            this.banana3 = new System.Windows.Forms.PictureBox();
+            this.banana2 = new System.Windows.Forms.PictureBox();
+            this.banana1 = new System.Windows.Forms.PictureBox();
+            this.banana4 = new System.Windows.Forms.PictureBox();
+            this.orange3 = new System.Windows.Forms.PictureBox();
+            this.orange1 = new System.Windows.Forms.PictureBox();
+            this.orange2 = new System.Windows.Forms.PictureBox();
+            this.orange4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.seven1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seven2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seven3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seven4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // seven1
+            // 
+            this.seven1.Image = ((System.Drawing.Image)(resources.GetObject("seven1.Image")));
+            this.seven1.Location = new System.Drawing.Point(200, 300);
+            this.seven1.Name = "seven1";
+            this.seven1.Size = new System.Drawing.Size(130, 130);
+            this.seven1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.seven1.TabIndex = 0;
+            this.seven1.TabStop = false;
+            // 
+            // cherry1
+            // 
+            this.cherry1.Image = ((System.Drawing.Image)(resources.GetObject("cherry1.Image")));
+            this.cherry1.Location = new System.Drawing.Point(200, 299);
+            this.cherry1.Name = "cherry1";
+            this.cherry1.Size = new System.Drawing.Size(130, 130);
+            this.cherry1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cherry1.TabIndex = 2;
+            this.cherry1.TabStop = false;
+            // 
+            // seven2
+            // 
+            this.seven2.Image = ((System.Drawing.Image)(resources.GetObject("seven2.Image")));
+            this.seven2.Location = new System.Drawing.Point(400, 299);
+            this.seven2.Name = "seven2";
+            this.seven2.Size = new System.Drawing.Size(130, 130);
+            this.seven2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.seven2.TabIndex = 5;
+            this.seven2.TabStop = false;
+            // 
+            // seven3
+            // 
+            this.seven3.Image = ((System.Drawing.Image)(resources.GetObject("seven3.Image")));
+            this.seven3.Location = new System.Drawing.Point(600, 299);
+            this.seven3.Name = "seven3";
+            this.seven3.Size = new System.Drawing.Size(130, 130);
+            this.seven3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.seven3.TabIndex = 6;
+            this.seven3.TabStop = false;
+            // 
+            // seven4
+            // 
+            this.seven4.Image = ((System.Drawing.Image)(resources.GetObject("seven4.Image")));
+            this.seven4.Location = new System.Drawing.Point(800, 299);
+            this.seven4.Name = "seven4";
+            this.seven4.Size = new System.Drawing.Size(130, 130);
+            this.seven4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.seven4.TabIndex = 7;
+            this.seven4.TabStop = false;
+            // 
+            // cherry3
+            // 
+            this.cherry3.Image = ((System.Drawing.Image)(resources.GetObject("cherry3.Image")));
+            this.cherry3.Location = new System.Drawing.Point(600, 300);
+            this.cherry3.Name = "cherry3";
+            this.cherry3.Size = new System.Drawing.Size(130, 130);
+            this.cherry3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cherry3.TabIndex = 8;
+            this.cherry3.TabStop = false;
+            // 
+            // cherry2
+            // 
+            this.cherry2.Image = ((System.Drawing.Image)(resources.GetObject("cherry2.Image")));
+            this.cherry2.Location = new System.Drawing.Point(400, 299);
+            this.cherry2.Name = "cherry2";
+            this.cherry2.Size = new System.Drawing.Size(130, 130);
+            this.cherry2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cherry2.TabIndex = 9;
+            this.cherry2.TabStop = false;
+            // 
+            // cherry4
+            // 
+            this.cherry4.Image = ((System.Drawing.Image)(resources.GetObject("cherry4.Image")));
+            this.cherry4.Location = new System.Drawing.Point(800, 299);
+            this.cherry4.Name = "cherry4";
+            this.cherry4.Size = new System.Drawing.Size(130, 130);
+            this.cherry4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cherry4.TabIndex = 10;
+            this.cherry4.TabStop = false;
+            // 
+            // grape3
+            // 
+            this.grape3.Image = ((System.Drawing.Image)(resources.GetObject("grape3.Image")));
+            this.grape3.Location = new System.Drawing.Point(600, 299);
+            this.grape3.Name = "grape3";
+            this.grape3.Size = new System.Drawing.Size(130, 130);
+            this.grape3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.grape3.TabIndex = 11;
+            this.grape3.TabStop = false;
+            // 
+            // grape4
+            // 
+            this.grape4.Image = ((System.Drawing.Image)(resources.GetObject("grape4.Image")));
+            this.grape4.Location = new System.Drawing.Point(800, 299);
+            this.grape4.Name = "grape4";
+            this.grape4.Size = new System.Drawing.Size(130, 130);
+            this.grape4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.grape4.TabIndex = 12;
+            this.grape4.TabStop = false;
+            // 
+            // grape2
+            // 
+            this.grape2.Image = ((System.Drawing.Image)(resources.GetObject("grape2.Image")));
+            this.grape2.Location = new System.Drawing.Point(400, 299);
+            this.grape2.Name = "grape2";
+            this.grape2.Size = new System.Drawing.Size(130, 130);
+            this.grape2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.grape2.TabIndex = 13;
+            this.grape2.TabStop = false;
+            // 
+            // grape1
+            // 
+            this.grape1.Image = ((System.Drawing.Image)(resources.GetObject("grape1.Image")));
+            this.grape1.Location = new System.Drawing.Point(200, 299);
+            this.grape1.Name = "grape1";
+            this.grape1.Size = new System.Drawing.Size(130, 130);
+            this.grape1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.grape1.TabIndex = 14;
+            this.grape1.TabStop = false;
+            // 
+            // banana3
+            // 
+            this.banana3.Image = ((System.Drawing.Image)(resources.GetObject("banana3.Image")));
+            this.banana3.Location = new System.Drawing.Point(600, 299);
+            this.banana3.Name = "banana3";
+            this.banana3.Size = new System.Drawing.Size(130, 130);
+            this.banana3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banana3.TabIndex = 15;
+            this.banana3.TabStop = false;
+            // 
+            // banana2
+            // 
+            this.banana2.Image = ((System.Drawing.Image)(resources.GetObject("banana2.Image")));
+            this.banana2.Location = new System.Drawing.Point(400, 300);
+            this.banana2.Name = "banana2";
+            this.banana2.Size = new System.Drawing.Size(130, 130);
+            this.banana2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banana2.TabIndex = 16;
+            this.banana2.TabStop = false;
+            // 
+            // banana1
+            // 
+            this.banana1.Image = ((System.Drawing.Image)(resources.GetObject("banana1.Image")));
+            this.banana1.Location = new System.Drawing.Point(200, 300);
+            this.banana1.Name = "banana1";
+            this.banana1.Size = new System.Drawing.Size(130, 130);
+            this.banana1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banana1.TabIndex = 17;
+            this.banana1.TabStop = false;
+            // 
+            // banana4
+            // 
+            this.banana4.Image = ((System.Drawing.Image)(resources.GetObject("banana4.Image")));
+            this.banana4.Location = new System.Drawing.Point(800, 300);
+            this.banana4.Name = "banana4";
+            this.banana4.Size = new System.Drawing.Size(130, 130);
+            this.banana4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banana4.TabIndex = 18;
+            this.banana4.TabStop = false;
+            // 
+            // orange3
+            // 
+            this.orange3.Image = ((System.Drawing.Image)(resources.GetObject("orange3.Image")));
+            this.orange3.Location = new System.Drawing.Point(600, 300);
+            this.orange3.Name = "orange3";
+            this.orange3.Size = new System.Drawing.Size(130, 130);
+            this.orange3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orange3.TabIndex = 19;
+            this.orange3.TabStop = false;
+            // 
+            // orange1
+            // 
+            this.orange1.Image = ((System.Drawing.Image)(resources.GetObject("orange1.Image")));
+            this.orange1.Location = new System.Drawing.Point(200, 299);
+            this.orange1.Name = "orange1";
+            this.orange1.Size = new System.Drawing.Size(130, 130);
+            this.orange1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orange1.TabIndex = 20;
+            this.orange1.TabStop = false;
+            // 
+            // orange2
+            // 
+            this.orange2.Image = ((System.Drawing.Image)(resources.GetObject("orange2.Image")));
+            this.orange2.Location = new System.Drawing.Point(400, 299);
+            this.orange2.Name = "orange2";
+            this.orange2.Size = new System.Drawing.Size(130, 130);
+            this.orange2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orange2.TabIndex = 21;
+            this.orange2.TabStop = false;
+            // 
+            // orange4
+            // 
+            this.orange4.Image = ((System.Drawing.Image)(resources.GetObject("orange4.Image")));
+            this.orange4.Location = new System.Drawing.Point(800, 300);
+            this.orange4.Name = "orange4";
+            this.orange4.Size = new System.Drawing.Size(130, 130);
+            this.orange4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orange4.TabIndex = 22;
+            this.orange4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1096, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 63);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Play!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1096, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 63);
+            this.button2.TabIndex = 24;
+            this.button2.Text = " More columns";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.orange4);
+            this.Controls.Add(this.orange2);
+            this.Controls.Add(this.orange1);
+            this.Controls.Add(this.orange3);
+            this.Controls.Add(this.banana4);
+            this.Controls.Add(this.banana1);
+            this.Controls.Add(this.banana2);
+            this.Controls.Add(this.banana3);
+            this.Controls.Add(this.grape1);
+            this.Controls.Add(this.grape2);
+            this.Controls.Add(this.grape4);
+            this.Controls.Add(this.grape3);
+            this.Controls.Add(this.cherry4);
+            this.Controls.Add(this.cherry2);
+            this.Controls.Add(this.cherry3);
+            this.Controls.Add(this.seven4);
+            this.Controls.Add(this.seven3);
+            this.Controls.Add(this.seven2);
+            this.Controls.Add(this.cherry1);
+            this.Controls.Add(this.seven1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.seven1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seven2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seven3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seven4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cherry4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grape1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banana4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orange4)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox seven1;
+        private System.Windows.Forms.PictureBox cherry1;
+        private System.Windows.Forms.PictureBox seven2;
+        private System.Windows.Forms.PictureBox seven3;
+        private System.Windows.Forms.PictureBox seven4;
+        private System.Windows.Forms.PictureBox cherry3;
+        private System.Windows.Forms.PictureBox cherry2;
+        private System.Windows.Forms.PictureBox cherry4;
+        private System.Windows.Forms.PictureBox grape3;
+        private System.Windows.Forms.PictureBox grape4;
+        private System.Windows.Forms.PictureBox grape2;
+        private System.Windows.Forms.PictureBox grape1;
+        private System.Windows.Forms.PictureBox banana3;
+        private System.Windows.Forms.PictureBox banana2;
+        private System.Windows.Forms.PictureBox banana1;
+        private System.Windows.Forms.PictureBox banana4;
+        private System.Windows.Forms.PictureBox orange3;
+        private System.Windows.Forms.PictureBox orange1;
+        private System.Windows.Forms.PictureBox orange2;
+        private System.Windows.Forms.PictureBox orange4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- kouloxeris/Form1.cs
+++ kouloxeris/Form1.cs
@@ -12,9 +12,39 @@ namespace kouloxeris
 {
     public partial class Form1 : Form
     {
+       
         public Form1()
         {
             InitializeComponent();
+            //τα κανω disable για να τα ενεργοποιησει ο χρηστης οποτε θελει με το πατημα ενος κουμπιου
+            orange1.Visible = false;
+            orange2.Visible = false;
+            orange3.Visible = false;
+            orange4.Visible = false;
+            banana4.Visible = false;
+            cherry4.Visible = false;
+            seven4.Visible = false;
+            grape4.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //βαζει περισσοτερες στηλες
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            orange1.Visible =true; 
+            orange2.Visible =true; 
+            orange3.Visible =true; 
+            orange4.Visible =true; 
+            banana4.Visible =true;
+            cherry4.Visible = true;
+            seven4.Visible = true;
+            grape4.Visible = true;
         }
     }
 }
